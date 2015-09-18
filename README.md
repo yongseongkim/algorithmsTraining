08e0edf99dbb8983aa6115de9a070ed832b3a9d2
yongseongkim <kys911015@gmail.com>
2015-09-18 09:35:01 +0000

    josephus

---
+++ README.md
@@ -0,0 +1,7 @@
+RockFestival: https://algospot.com/judge/problem/read/FESTIVAL
+Picnic: https://algospot.com/judge/problem/read/PICNIC
+Packing:
+Josephus: https://algospot.com/judge/problem/read/JOSEPHUS
+ReadPi: https://algospot.com/judge/problem/read/PI
+Traversal: https://algospot.com/judge/problem/read/TRAVERSAL
+Dictionary: https://algospot.com/judge/problem/read/DICTIONARY
---
+++ josephus.py
@@ -0,0 +1,17 @@
+total_case = input()
+case_index = 0
+
+while case_index < total_case:
+    NK = [int(i) for i in raw_input().split()]
+    N, K = NK[0], NK[1]
+
+    player = [i for i in xrange(2, N + 1)]
+    died = 0
+    while len(player) != 2:
+        died = died + K - 1
+        while died >= len(player):
+            died = died - len(player)
+        player.pop(died)
+    for i in player: print i,
+    print ''
+    case_index = case_index + 1
